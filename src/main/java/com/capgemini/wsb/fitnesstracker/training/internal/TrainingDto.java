d024ee4c1a9f591f13dc7011e46755268e6f90fc
MarceliG <marcel9817@gmail.com>
2024-11-06 20:09:20 +0100

    createTraining in progres

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingDto.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingDto.java
@@ -5,11 +5,11 @@ import java.util.Date;
 import com.capgemini.wsb.fitnesstracker.user.api.User;
 
 public record TrainingDto(
-        Long id,
-        User user,
-        Date startTime,
-        Date endTime,
-        ActivityType activityType,
-        double distance,
-        double averageSpeed) {
+                Long id,
+                User user,
+                Date startTime,
+                Date endTime,
+                ActivityType activityType,
+                double distance,
+                double averageSpeed) {
 }
